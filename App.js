140ee079c16ddd7decb0ac0d383e79ed8a0f7f93
tingzhouu <limtingzhou@gmail.com>
2020-08-05 22:29:53 +0800

    remove non-essential parts of the component

--- App.js
+++ App.js
@@ -1,20 +1,11 @@
 import React from 'react';
-import {
-  Text,
-  View,
-  TextInput,
-  Button,
-  Alert,
-  StyleSheet,
-  SafeAreaView,
-} from 'react-native';
+import {Text, TextInput, StyleSheet, SafeAreaView} from 'react-native';
 import {useForm, Controller} from 'react-hook-form';
 
 export default function App() {
-  const {control, handleSubmit, errors, formState} = useForm({
+  const {control, errors, formState} = useForm({
     mode: 'onChange',
   });
-  const onSubmit = (data) => console.log(data);
 
   return (
     <SafeAreaView>
@@ -37,29 +28,6 @@ export default function App() {
         defaultValue=""
       />
       {errors.firstName && <Text>This is required.</Text>}
-
-      <Controller
-        control={control}
-        render={({onChange, onBlur, value}) => (
-          <TextInput
-            accessibilityLabel="Last Name Field"
-            style={styles.input}
-            onBlur={onBlur}
-            onChangeText={(value) => onChange(value)}
-            value={value}
-          />
-        )}
-        name="lastName"
-        rules={{required: true}}
-        defaultValue=""
-      />
-
-      <Button
-        disabled={!formState.isValid}
-        accessibilityLabel="Submit Button"
-        title="Submit"
-        onPress={handleSubmit(onSubmit)}
-      />
     </SafeAreaView>
   );
 }
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import App from '../App';
 
-import {render} from '@testing-library/react-native';
+import {render, waitFor} from '@testing-library/react-native';
 
 describe('checking formState.isValid', () => {
-  it('should initialize with an invalid state', () => {
+  it('should initialize with an invalid state', async () => {
     const {getByText} = render(<App />);
-    getByText('formState: "Invalid"');
-    // getByText('formState: "Valid"'); // this will pass the test, but is not what we expect
-  });
-
-  it('should have a disabled submit button', () => {
-    const {getByA11yLabel} = render(<App />);
-    const submitButton = getByA11yLabel('Submit Button');
-    expect(submitButton).toBeDisabled();
-    // expect(submitButton).not.toBeDisabled(); // this will pass the test, but is not what we expect
+    await waitFor(() => getByText('formState: "Invalid"'));
   });
 });
