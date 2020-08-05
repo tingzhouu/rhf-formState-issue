1330bb6ca09381df54d1b4235cb457e9e34c2388
tingzhouu <limtingzhou@gmail.com>
2020-08-05 12:10:30 +0800

    add line on how to pass test

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -7,11 +7,13 @@ describe('checking formState.isValid', () => {
   it('should initialize with an invalid state', () => {
     const {getByText} = render(<App />);
     getByText('formState: "Invalid"');
+    // getByText('formState: "Valid"'); // this will pass the test, but is not what we expect
   });
 
   it('should have a disabled submit button', () => {
     const {getByA11yLabel} = render(<App />);
     const submitButton = getByA11yLabel('Submit Button');
     expect(submitButton).toBeDisabled();
+    // expect(submitButton).not.toBeDisabled(); // this will pass the test, but is not what we expect
   });
 });
