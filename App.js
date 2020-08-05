fdc34a41b62122d4a77cbb7144da8ca35f381afb
tingzhouu <limtingzhou@gmail.com>
2020-08-05 12:09:33 +0800

    add invalid form state demo in app and jest test

--- App.js
+++ App.js
@@ -1,114 +1,74 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
   Text,
-  StatusBar,
+  View,
+  TextInput,
+  Button,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
 } from 'react-native';
+import {useForm, Controller} from 'react-hook-form';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+export default function App() {
+  const {control, handleSubmit, errors, formState} = useForm({
+    mode: 'onChange',
+  });
+  const onSubmit = (data) => console.log(data);
 
-const App: () => React$Node = () => {
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+    <SafeAreaView>
+      <Text>
+        {formState.isValid ? 'formState: "Valid"' : 'formState: "Invalid"'}
+      </Text>
+      <Controller
+        control={control}
+        render={({onChange, onBlur, value}) => (
+          <TextInput
+            accessibilityLabel="First Name Field"
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="firstName"
+        rules={{required: true}}
+        defaultValue=""
+      />
+      {errors.firstName && <Text>This is required.</Text>}
+
+      <Controller
+        control={control}
+        render={({onChange, onBlur, value}) => (
+          <TextInput
+            accessibilityLabel="Last Name Field"
+            style={styles.input}
+            onBlur={onBlur}
+            onChangeText={(value) => onChange(value)}
+            value={value}
+          />
+        )}
+        name="lastName"
+        rules={{required: true}}
+        defaultValue=""
+      />
+
+      <Button
+        disabled={!formState.isValid}
+        accessibilityLabel="Submit Button"
+        title="Submit"
+        onPress={handleSubmit(onSubmit)}
+      />
+    </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  input: {
+    height: 30,
+    width: 300,
+    borderColor: 'gray',
+    borderWidth: 1,
   },
 });
-
-export default App;
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,17 @@
-/**
- * @format
- */
-
-import 'react-native';
 import React from 'react';
 import App from '../App';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+import {render} from '@testing-library/react-native';
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+describe('checking formState.isValid', () => {
+  it('should initialize with an invalid state', () => {
+    const {getByText} = render(<App />);
+    getByText('formState: "Invalid"');
+  });
+
+  it('should have a disabled submit button', () => {
+    const {getByA11yLabel} = render(<App />);
+    const submitButton = getByA11yLabel('Submit Button');
+    expect(submitButton).toBeDisabled();
+  });
 });
