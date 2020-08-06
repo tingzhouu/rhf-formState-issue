f247148d1f5517b9f3fc2c7ddd3c4b560ebb44e2
tingzhouu <limtingzhou@gmail.com>
2020-08-06 10:46:06 +0800

    add react native specific configuration for undefined window object

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import App from '../App';
 
-import {render, waitFor} from '@testing-library/react-native';
+import {render, waitFor} from 'react-native-testing-library';
 
 describe('checking formState.isValid', () => {
   it('should initialize with an invalid state', async () => {
